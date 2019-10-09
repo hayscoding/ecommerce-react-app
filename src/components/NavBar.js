99eb5755ba2a75e18d6ee6185792ff036308745c
Hays Stanford <stanfordhays@gmail.com>
2019-10-08 22:57:57 -0500

    NavBar now shows a well styled navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-        <p>Learn React</p>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
-
-export default App;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
+import NavBar from '../components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
