13e5655aae508de6b7fe54011c0f7151d34d1d3e
Hays Stanford <stanfordhays@gmail.com>
2019-10-08 22:51:47 -0500

    added NavBar component & test in App.test.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Learn React</p>
+    </div>
+  );
+}
+
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../pages/App';
+import NavBar from '../components/NavBar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('components render without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NavBar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
